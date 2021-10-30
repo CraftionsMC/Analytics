e9deb706299756c94e7e2d58db7f15c9e75252cb
MCTzOCK <dev@mctzock.de>
2021-10-30 14:12:47 +0200

    Updated Client

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -6,42 +6,6 @@
 import * as React from "react";
 
 export default function Home() {
-  fetch("/api/v1/analyze", {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-    body: JSON.stringify({
-      data: {
-        version: "1.0",
-        location: {
-          protocol: location.protocol,
-          host: location.host,
-          path: location.pathname,
-          port: location.port,
-          origin: location.origin,
-          href: location.href,
-        },
-        siteData: {
-          title: document.title,
-        },
-        clientData: {
-          browser: navigator.userAgent,
-          screenColors: screen.colorDepth + "-bit",
-          language: navigator.language,
-          darkMode:
-            window.matchMedia &&
-            window.matchMedia("(prefers-color-scheme: dark)").matches,
-        },
-      },
-    }),
-  })
-    .then((r) => r.json())
-    .then((j) => console.log(j));
   return (
     <>
       <h1>Home</h1>
